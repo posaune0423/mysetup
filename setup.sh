4d5707002ffc4531a51062c99cf10131a5744f73
Asuma Yamada <y.asuma0423@icloud.com>
2021-01-17 18:42:49 +0900

    docs: add OSX  Basic settings

--- setup.sh
+++ setup.sh
@@ -1,6 +1,43 @@
 #!/bin/sh
 
 
+# OS settings
+
+# make tab key able to move focus
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# increase key repeat
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+
+# make CapsLock Control
+# get string like : 1452-630-0 for keyboard_id (ref: http://freewing.starfree.jp/software/macos_keyboard_setting_terminal_commandline)
+keyboard_id="$(ioreg -c AppleEmbeddedKeyboard -r | grep -Eiw "VendorID|ProductID" | awk '{ print $4 }' | paste -s -d'-\n' -)-0"
+defaults -currentHost write -g com.apple.keyboard.modifiermapping.${keyboard_id} -array-add "
+<dict>
+  <key>HIDKeyboardModifierMappingDst</key>\
+  <integer>30064771300</integer>\
+  <key>HIDKeyboardModifierMappingSrc</key>\
+  <integer>30064771129</integer>\
+</dict>
+"
+
+
+defaults write -g com.apple.trackpad.scaling 8.5  # set trackpad speed
+defaults write -g com.apple.mouse.scaling 8.5  # set mouse speed
+defaults write -g com.apple.mouse.tapBehavior -int 1 # when tapping, let it click
+
+defaults write com.apple.finder AppleShowAllFiles YES  # show hidden files in finder
+defaults write com.apple.dock autohide -bool true  # Automatically hide or show the Dock
+defaults write com.apple.dock persistent-apps -array  # Wipe all app icons from the Dock
+defaults write com.apple.dock magnification -bool true  # Magnificate the Dock
+
+# resstart these apps for applying above settings
+killall Finder
+killall Dock
+
+
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
