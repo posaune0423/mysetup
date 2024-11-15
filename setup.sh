be927753ae8db9ff8dac6bffe0bb7a32b2b5ad28
Asuma Yamada <asuma.a.yamada@dena.com>
2024-11-15 18:09:47 +0900

    fix: update

--- setup.sh
+++ setup.sh
@@ -1,108 +1,173 @@
-#!/bin/sh
+#!/bin/bash
 
+# Exit on error
+set -e
 
-# OS settings
+# Ask for the administrator password upfront
+sudo -v
 
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3  # make tab key able to move focus
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# increase key repeat
+echo "Starting macOS setup script..."
+
+# Function to check if command succeeded
+check_status() {
+    if [ $? -eq 0 ]; then
+        echo "✅ $1"
+    else
+        echo "❌ $1 failed"
+    fi
+}
+
+#################################################
+# OS Settings
+#################################################
+
+echo "Configuring macOS settings..."
+
+# Keyboard settings
+osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
+check_status "Keyboard settings"
 
+# CapsLock to Control
+for keyboard_id in $(ioreg -c AppleEmbeddedKeyboard -r | grep -Eiw "VendorID|ProductID" | awk '{ print $4 }' | paste -s -d'-\n' -)-0; do
+    defaults -currentHost write -g com.apple.keyboard.modifiermapping.${keyboard_id} -array-add "
+    <dict>
+        <key>HIDKeyboardModifierMappingDst</key>
+        <integer>30064771300</integer>
+        <key>HIDKeyboardModifierMappingSrc</key>
+        <integer>30064771129</integer>
+    </dict>
+    "
+done
+check_status "CapsLock to Control mapping"
 
-# make CapsLock Control
-# get string like : 1452-630-0 for keyboard_id (ref: http://freewing.starfree.jp/software/macos_keyboard_setting_terminal_commandline)
-keyboard_id="$(ioreg -c AppleEmbeddedKeyboard -r | grep -Eiw "VendorID|ProductID" | awk '{ print $4 }' | paste -s -d'-\n' -)-0"
-defaults -currentHost write -g com.apple.keyboard.modifiermapping.${keyboard_id} -array-add "
-<dict>
-  <key>HIDKeyboardModifierMappingDst</key>\
-  <integer>30064771300</integer>\
-  <key>HIDKeyboardModifierMappingSrc</key>\
-  <integer>30064771129</integer>\
-</dict>
-"
-
-
-defaults write -g com.apple.trackpad.scaling 8.5  # set trackpad speed
-defaults write -g com.apple.mouse.scaling 8.5  # set mouse speed
-defaults write -g com.apple.mouse.tapBehavior -int 1 # when tapping, let it click
-
-## Three finger drag
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true && \
+# Trackpad settings
+defaults write -g com.apple.trackpad.scaling 8.5
+defaults write -g com.apple.mouse.scaling 8.5
+defaults write -g com.apple.mouse.tapBehavior -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+check_status "Trackpad settings"
 
+# Finder settings
+defaults write com.apple.screencapture type jpg
+defaults write com.apple.finder AppleShowAllFiles YES
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+check_status "Finder settings"
 
-defaults write com.apple.screencapture type jpg # save screanshot as jpg extension
-defaults write com.apple.finder AppleShowAllFiles YES  # show hidden files in finder
-defaults write com.apple.dock autohide -bool true  # Automatically hide or show the Dock
-defaults write com.apple.dock persistent-apps -array  # Wipe all app icons from the Dock
-defaults write com.apple.dock magnification -bool true  # Magnificate the Dock
+# Dock settings
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock persistent-apps -array
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock tilesize -int 36
+check_status "Dock settings"
 
+#################################################
+# Install Applications
+#################################################
 
-# install homebrew if not installed
-if [ ! -e /usr/local/bin/brew ]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Setting up applications..."
+
+# Install Homebrew if not installed
+if ! command -v brew &>/dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Add Homebrew to PATH for Apple Silicon Macs
+    if [[ $(uname -m) == 'arm64' ]]; then
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+fi
+check_status "Homebrew installation"
+
+# Install Rosetta 2 for Apple Silicon Macs
+if [[ $(uname -m) == 'arm64' ]]; then
+    echo "Installing Rosetta 2..."
+    softwareupdate --install-rosetta --agree-to-license
+    check_status "Rosetta 2 installation"
 fi
 
-if [ -f ~/.zshrc ]; then
-  echo export HOMEBREW_CASK_OPTS="--appdir=/Applications" >> ~/.zshrc
-else
-  touch ~/.zshrc
-  echo export HOMEBREW_CASK_OPTS="--appdir=/Applications" >> ~/.zshrc
+# Update Homebrew
+echo "Updating Homebrew..."
+brew update
+brew upgrade
+check_status "Homebrew update"
+
+# CLI tools
+echo "Installing CLI tools..."
+cli_tools=(
+    "bat"
+    "tree"
+    "tig"
+    "nvim"
+    "tmux"
+    "hub"
+    "neofetch"
+    "peco"
+    "eza"
+)
+
+for tool in "${cli_tools[@]}"; do
+    brew install "$tool"
+done
+check_status "CLI tools installation"
+
+# GUI Applications
+echo "Installing GUI applications..."
+gui_apps=(
+    "google-japanese-ime"
+    "google-chrome"
+    "slack"
+    "zoom"
+    "alfred"
+    "spectacle"
+    "wezterm"
+    "cursor"
+    "docker"
+    "monitorcontrol"
+)
+
+for app in "${gui_apps[@]}"; do
+    brew install --cask "$app"
+done
+check_status "GUI applications installation"
+
+# Install Mac App Store CLI and apps
+if ! command -v mas &>/dev/null; then
+    brew install mas
 fi
 
+echo "Installing Mac App Store apps..."
+mas_apps=(
+    "1429033973:RunCat"
+)
 
-# install zsh plugin
-brew install zsh-completions
-brew install zsh-autosuggestions
-brew install zsh-syntax-highlighting
+for app in "${mas_apps[@]}"; do
+    mas install "${app%%:*}"
+done
+check_status "Mac App Store apps installation"
 
-# install cli commands from brew
-brew install tree
-brew install tig
-brew install nvim
-brew install tmux
-brew install hub
-brew install neofetch
-brew install peco
-brwe install exa
+# Cleanup
+echo "Cleaning up..."
+brew cleanup
+check_status "Cleanup"
 
-# install packages from brew
-# brew install pyenv-virtualenv
-brew install pyenv
-brew install nodebrew
-brew yarn
-
-# install utils
-brew install ffmpeg
-
-
-# install desktop apps
-brew install google-japanese-ime --cask
-brew install google-chrome --cask
-brew install slack --cask
-brew install zoom --cask
-brew install alfred --cask
-brew install spectacle --cask
-brew install iterm2 --cask
-brew install visual-studio-code --cask
-brew install sequel-ace --cask
-brew install docker --cask
-brew install monitorcontrol --cask
-
-
-
-
-if [ ! -e /usr/local/bin/mas ]; then
-  brew install mas
-fi
-
-# install by mas
-mas install 1429033973   # RunCat
-
-
-# resstart these apps for applying above settings
+# Restart system processes
+echo "Restarting system processes..."
 killall Finder
 killall Dock
 
-sudo shutdown -r now
+echo "Setup complete! Some changes require a restart to take effect."
+echo "Would you like to restart now? (y/n)"
+read -r response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+    sudo shutdown -r now
+fi
